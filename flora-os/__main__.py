8a5db6700ddc90c4a53c9bb4b1633c56e40738cb
Ben Wiswell <benjwiswell@gmail.com>
2026-03-05 16:04:15 -0500

    feat: configure for testing

--- flora-os/__main__.py
+++ flora-os/__main__.py
@@ -1,8 +1,15 @@
 import sys
 
+from .network import Client, Server
+
 
 module = sys.argv[1]
 
 
 if __name__ == '__main__':
-    print(module)
+    if module == 'traction':
+        Client.connect('traction')
+    elif module == 'sensors':
+        Client.connect('sensors')
+    else:
+        Server()
--- flora-os/network/server.py
+++ flora-os/network/server.py
@@ -16,6 +16,7 @@ class Server(Thread):
         Thread.__init__(self, self._start_server)
         self.sensors: Relay = None
         self.traction: Relay = None
+        self.run()
 
 
     ### HELPERS ###
