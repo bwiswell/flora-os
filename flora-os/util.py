58a8f3b4cacef730ebb51edfdf90554c6cbec11c
Ben Wiswell <benjwiswell@gmail.com>
2026-03-05 23:11:01 -0500

    fix: var name conflict in util

--- flora-os/util.py
+++ flora-os/util.py
@@ -7,8 +7,8 @@ Number = TypeVar('Number', int, float)
 def clip (val: Number, low: Number, high: Number) -> Number:
     return max(low, min(high, val))
 
-def clip_and_scale (val: float, scale: int, low: int, high: int) -> int:
-    return clip(scale(val, scale), low, high)
+def clip_and_scale (val: float, scalar: int, low: int, high: int) -> int:
+    return clip(scale(val, scalar), low, high)
 
 def scale (val: float, scale: int) -> int:
     return round(val * scale)
