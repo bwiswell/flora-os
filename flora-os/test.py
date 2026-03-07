70f8ccc6a88a4d75ebe58b67cdd0345d063f0f05
Ben Wiswell <benjwiswell@gmail.com>
2026-03-06 21:34:38 -0500

    fix: add motor bias and update test harness

--- flora-os/test.py
+++ flora-os/test.py
@@ -1,6 +1,27 @@
-from .common import FineMotor
+import time
+
+from .common import Expression, FineMotor, Mood
+from .sensors import Mouth
+
+
+last = Expression.OPEN
+m = Mouth()
+
+def callback (angle: int):
+    global last
+    last = Expression.CORNERS if last == Expression.OPEN else Expression.OPEN
+    time.sleep(1)
+    m.update(last, Mood.CURIOUS)
 
 
 def test ():
-        m = FineMotor('A', 3.0)
-        m.sweep(-90, 90, 15, print)
+    m.update(Expression.FLAT, Mood.NONE)
+    time.sleep(3)
+    m.update(Expression.SMILE, Mood.HAPPY)
+    time.sleep(3)
+    m.update(Expression.OPEN, Mood.CURIOUS)
+    s = FineMotor('B', 3.0, bias=-1)
+    s.sweep(-90, 90, 15, callback)
+    m.update(Expression.RESTING, Mood.NONE)
+    time.sleep(3)
+
