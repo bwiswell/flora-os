8571da553671ee7e04a43f88a9702fead03fe47f
Ben Wiswell <benjwiswell@gmail.com>
2026-03-06 21:34:38 -0500

    fix: add motor bias and update test harness

--- flora-os/common/fine_motor.py
+++ flora-os/common/fine_motor.py
@@ -8,25 +8,39 @@ Direction = Literal['clockwise', 'anticlockwise', 'shortest']
 
 class FineMotor:
     
-    def __init__ (self, port: str, ratio: float = 1.0, plimit: float = 1.0):
+    def __init__ (
+                self,
+                port: str,
+                ratio: float = 1.0,
+                bias: int = 0,
+                plimit: float = 1.0
+            ):
         self.motor = Motor(port)
         self.motor.plimit(plimit)
         self.ratio = ratio
+        self.bias = bias
         self.reset()
 
 
     ### HELPERS ###
-    def _from_motor_apos (self, apos: int, was_positive: bool) -> int:
-        if apos < 0 and was_positive:
+    def _from_motor_apos (
+                self,
+                apos: int,
+                was_positive: bool,
+                was_zero: bool
+            ) -> int:
+        if was_zero:
+            normalized = apos
+        elif apos < 0 and was_positive:
             normalized = 180 + (apos + 180)
         elif apos > 0 and not was_positive:
             normalized = -180 - (180 - apos)
         else:
             normalized = apos
-        return round(normalized / self.ratio)
+        return round(normalized / self.ratio) + self.bias
 
     def _to_motor_apos (self, apos: int) -> int:
-        scaled = round(apos * self.ratio)
+        scaled = round((apos - self.bias) * self.ratio)
         if scaled < -180:
             return 180 - (-180 - scaled)
         elif scaled > 180:
@@ -47,6 +61,7 @@ class FineMotor:
                 speed_limit: int = 100
             ) -> int:
         was_positive = degrees >= 0
+        was_zero = degrees == 0
         apos = self._to_motor_apos(degrees)
         for speed in [100, 75, 50, 40]:
             if speed_limit >= speed:
@@ -54,7 +69,8 @@ class FineMotor:
         self.motor.run_to_position(apos, max(30, speed_limit), blocking, dir)
         result = self._from_motor_apos(
             self.motor.get_aposition(),
-            was_positive
+            was_positive,
+            was_zero
         )
         print(f'target: {degrees}, result: {result}')
         return result
---
+++ flora-os/sensors/__init__.py
@@ -0,0 +1,4 @@
+from .head import Head
+from .mouth import Mouth
+from .sensors import Sensors
+from .sonar import Sonar
