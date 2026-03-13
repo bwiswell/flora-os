7bb6016f171577c73faf810b4771dfe2fb738bc9
Ben Wiswell <benjwiswell@gmail.com>
2026-03-13 01:19:19 -0400

    feat: start documenting some code with plans to break out slam into a separate package

--- flora_os/slam/helpers/bilinear_interpolation.py
+++ flora_os/slam/helpers/bilinear_interpolation.py
@@ -4,25 +4,58 @@ import numpy as np
 
 
 def bilinear_interpolation (grid: np.ndarray, xy: np.ndarray) -> np.ndarray:
-    n_pts = xy.shape[0]
-    inter = np.ndarray((n_pts), np.float64)
-    for i in range(n_pts):
-        x = xy[i, 0]
-        y = xy[i, 1]
-        i_a = math.floor(y)
-        i_b = i_a + 1
-        j_a = math.floor(x)
-        j_b = j_a + 1
-        if i_a < 0 or i_b >= grid.shape[0] or \
-                j_a < 0 or j_b >= grid.shape[1]:
-            inter[i] = 0.0
-            continue
-        q_a_a = grid[i_a, j_a]
-        q_b_a = grid[i_a, j_b]
-        q_a_b = grid[i_b, j_a]
-        q_b_b = grid[i_b, j_b]
-        w_a = (j_b - x) * q_a_a + (x - j_a) * q_b_a
-        w_b = (j_b - x) * q_a_b + (x - j_a) * q_b_b
-        w = (i_b - y) * w_a + (y - i_a) * w_b
-        inter[i] = w
+    '''
+    Returns an `ndarray` of interpolated occupancy values computed for
+    xy-coordinates found in `xy` and the current occupancy map `grid`.
+
+    Parameters:
+        grid (`ndarray`):
+            A 2D `ndarray` of occupancy values with shape (`h`, `w`), where `h`
+            is the map width and `w` is the map height.
+        xy (`ndarray`):
+            A 2D `ndarray` of xy-coordinates with shape (`n`, 2), where `n` is
+            the number of coordinate values, `x` values are stored in column 0,
+            and `y` values are stored in column 1.
+
+    Returns:
+        interpolated (`ndarray`):
+            A 1D `ndarray` of interpolated occupancy results with shape
+            (`n`, 1), where `n` is the number of valid (not out-of-bounds)
+            coordinates found in `xy`.
+    '''
+
+    # Get raw x and y coordinates
+    x, y = xy[:, 0], xy[:, 1]
+
+    # Get integer coordinates
+    x_a = np.floor(x).astype(np.int32)
+    x_b = x_a + 1
+    y_a = np.floor(y).astype(np.int32)
+    y_b = y_a + 1
+
+    # Initialize the output array
+    inter = np.ndarray(xy.shape[0], np.float64)
+
+    # Mask to detect coordinates that are out-of-bounds
+    mask = (x_a >= 0) & (x_b < grid.shape[1]) & \
+            (y_a >= 0) & (y_b < grid.shape[0])
+
+    # Find valid coordinates using the mask
+    valid_x_a, valid_x_b = x_a[mask], x_b[mask]
+    valid_y_a, valid_y_b = y_a[mask], y_b[mask]
+    valid_x, valid_y = x[mask], y[mask]
+    
+    # Find cell corners
+    q_a_a = grid[valid_y_a, valid_x_a]
+    q_a_b = grid[valid_y_a, valid_x_b]
+    q_b_a = grid[valid_y_b, valid_x_a]
+    q_b_b = grid[valid_y_b, valid_x_b]
+
+    # Compute x-coordinate interpolation weights
+    w_a = (valid_x_b - valid_x) * q_a_a + (valid_x - valid_x_a) * q_a_b
+    w_b = (valid_x_b - valid_x) * q_b_a + (valid_x - valid_x_a) * q_b_b
+
+    # Compute interpolated values
+    inter[mask] = (valid_y_b - valid_y) * w_a + (valid_y - valid_y_a) * w_b
+
     return inter
--- flora_os/slam/helpers/util.py
+++ flora_os/slam/helpers/util.py
@@ -4,17 +4,64 @@ import numpy as np
 
 
 def d_rotation_matrix (theta: float) -> np.ndarray:
-        c, s = math.cos(theta), math.sin(theta)
-        return np.array([[-s, c], [-c, -s]])
+    '''
+    Returns the 2D derivative of the standard rotation matrix for the given
+    angle `theta` (in radians).
+
+    Parameters:
+        theta (`float`):
+            The angle (in radians) to create rotation matrix derivative from.
+
+    Returns:
+        d_rotation_matrix (`ndarray`):
+            A 2D `ndarray` with shape (2, 2) representing the derivative of the
+            standard rotation transform matrix for `theta`
+
+            [[-`s`, `c`],\n
+             [`-c`, `-s`]]
+
+            where `c` is the cosine of `theta` and `s` is the sine of `theta`.
+    '''
+    c, s = math.cos(theta), math.sin(theta)
+    return np.array([[-s, c], [-c, -s]])
 
 
 def rotation_matrix (theta: float) -> np.ndarray:
+    '''
+    Returns the 2D rotation matrix for the given angle `theta` (in radians).
+
+    Parameters:
+        theta (`float`):
+            The angle (in radians) to create a rotation matrix from.
+
+    Returns:
+        rotation_matrix (`ndarray`):
+            A 2D `ndarray` with shape (2, 2) representing the standard rotation
+            transform matrix for `theta`
+
+            [[`c`, -`s`],\n
+             [`s`, `c`]]
+
+            where `c` is the cosine of `theta` and `s` is the sine of `theta`.
+    '''
     c, s = math.cos(theta), math.sin(theta)
     return np.array([[c, -s], [s, c]])
 
 
-def wrap_radians (val: float) -> float:
-    r = val
+def wrap_radians (theta: float) -> float:
+    '''
+    Returns an equivalent angle (in radians) to `val` in the range
+    (-`pi`, `pi`).
+
+    Parameters:
+        theta (`float`):
+            The angle (in radians) to convert.
+
+    Returns:
+        theta_prime (`float`):
+            An equivalent angle (in radians) in the range (-`pi`, `pi`).
+    '''
+    r = theta
     while r < -math.pi:
         r += 2 * math.pi
     while r > math.pi:
